d7d01299fbb766f9502444afbffa7c9317c18c83
Tiago Cunha <tiago92cunha@gmail.com>
2025-05-07 22:41:08 +0100

    Add initial schemas

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { index, pgTableCreator } from "drizzle-orm/pg-core";
 
 /**
@@ -27,3 +27,46 @@ export const posts = createTable(
   }),
   (t) => [index("name_idx").on(t.name)],
 );
+
+export const image = createTable("image", (d) => ({
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  url: d.varchar({ length: 256 }),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+}));
+
+export const book = createTable("book", (d) => ({
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  title: d.varchar({ length: 256 }),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+}));
+
+export const bookImage = createTable("book_image", (d) => ({
+  id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+  bookId: d.integer().references(() => book.id),
+  imageId: d.integer().references(() => image.id),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+}));
+
+export const bookRelations = relations(book, ({ many }) => ({
+  images: many(bookImage),
+}));
+
+export const bookImageRelations = relations(bookImage, ({ one }) => ({
+  book: one(book, {
+    fields: [bookImage.bookId],
+    references: [book.id],
+  }),
+  image: one(image, {
+    fields: [bookImage.imageId],
+    references: [image.id],
+  }),
+}));
