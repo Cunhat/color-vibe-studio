e04596cf7ac3dc36f6d11f08b75958f00f279502
Tiago Cunha <tiago92cunha@gmail.com>
2025-07-25 17:36:53 +0100

    Add Book form

--- src/modules/dashboard/components/add-to-book.tsx
+++ src/modules/dashboard/components/add-to-book.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogClose,
@@ -11,8 +11,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
-import { Book } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import {
   Select,
   SelectContent,
@@ -20,15 +27,36 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import type { ImageWithPrompt } from "@/lib/schemas";
 import { api } from "@/trpc/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Book } from "lucide-react";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
 
 type AddToBookProps = {
   selectedImages: Array<string>;
 };
 
+const AddToBookSchema = z.object({
+  book: z.string().min(1, { message: "Selecting a book is required!" }),
+});
+
+type AddToBookFormType = z.infer<typeof AddToBookSchema>;
+
 export default function AddToBook({ selectedImages }: AddToBookProps) {
   const booksQuery = api.book.getBooks.useQuery();
+
+  const form = useForm<AddToBookFormType>({
+    resolver: zodResolver(AddToBookSchema),
+    defaultValues: {
+      book: "",
+    },
+  });
+
+  function onSubmit(data: AddToBookFormType) {
+    console.log(data);
+  }
 
   return (
     <Dialog>
@@ -45,26 +73,49 @@ export default function AddToBook({ selectedImages }: AddToBookProps) {
             Select a book to add the selected images to.
           </DialogDescription>
         </DialogHeader>
-        <Select>
-          <SelectTrigger className="w-full">
-            <SelectValue placeholder="Theme" />
-          </SelectTrigger>
-          <SelectContent>
-            {booksQuery.data?.map((book) => {
-              return (
-                <SelectItem key={book.id} value={book.id}>
-                  {book.title}
-                </SelectItem>
-              );
-            })}
-          </SelectContent>
-        </Select>
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DialogClose>
-          <Button type="submit">Add to Book</Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            id="add-to-book-form"
+            className="space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name="book"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Book</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select a book" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {booksQuery.data?.map((book) => {
+                        return (
+                          <SelectItem key={book.id} value={book.id}>
+                            {book.title}
+                          </SelectItem>
+                        );
+                      })}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Add to Book</Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
